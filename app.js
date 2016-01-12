db4ef85306f24ffadeacaffce8485524c993aba6
ealhad <34lh4d@gmail.com>
2016-01-12 15:19:52 +0100

    Proof-of-concept
    The master page can communicate with its child. Nice

---
+++ app.js
@@ -0,0 +1,29 @@
+var client_window;
+
+$(document)
+  .ready(function() {
+    console.log('start');
+    client_window = window.open();
+
+    $.get('client.html', function(html) {
+      $(client_window.document.body).html(html);
+    }, 'html');
+
+    setInterval(update, 100);
+  });
+
+function update() {
+  $(client_window.document.body).find('.timer').html($('.timer .edit').val());
+  $(client_window.document.body)
+    .find('.team1 .name')
+    .html($('.team1 .name .edit').val());
+  $(client_window.document.body)
+    .find('.team1 .score')
+    .html($('.team1 .score .edit').val());
+  $(client_window.document.body)
+    .find('.team2 .name')
+    .html($('.team2 .name .edit').val());
+  $(client_window.document.body)
+    .find('.team2 .score')
+    .html($('.team2 .score .edit').val());
+}
