5f9de48d6c818383b44c85a2ac78aa56fc3218a8
ealhad <34lh4d@gmail.com>
2016-01-13 17:23:48 +0100

    Bases du design du machin

--- app.js
+++ app.js
@@ -5,26 +5,17 @@ $(document)
     console.log('start');
     client_window = window.open();
 
-    $.get('client.html', function(html) {
-      console.log(html);
-      client_window.document.write(html);
-    }, 'html');
+    $.get('client.html',
+      function(html) {
+        client_window.document.write(html);
+      }, 'html');
 
-    setInterval(update, 100);
+    // example of content update
+    setInterval(function() {
+      update('#team1_name');
+    }, 100);
   });
 
-function update() {
-  $(client_window.document.body).find('.timer').html($('.timer .edit').val());
-  $(client_window.document.body)
-    .find('.team1 .name')
-    .html($('.team1 .name .edit').val());
-  $(client_window.document.body)
-    .find('.team1 .score')
-    .html($('.team1 .score .edit').val());
-  $(client_window.document.body)
-    .find('.team2 .name')
-    .html($('.team2 .name .edit').val());
-  $(client_window.document.body)
-    .find('.team2 .score')
-    .html($('.team2 .score .edit').val());
+function update(id) {
+  $(client_window.document.body).find(id).html($(id).val());
 }
