3b6c15473d1a617b095140c1463fab24b4ce4af1
BuonOmo <buonomo.ulysse@gmail.com>
2016-05-04 01:42:57 +0200

    ajout du spy-scroll

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('#toggle_client').click(function() {
        $('#client_controls').slideDown();
        document.getElementById('client_controls').removeAttribute('hidden');
-       $(this).slideUp();
+       $('#jumbotron').slideUp();
        master_init();
     });
 });
